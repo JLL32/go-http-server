7f93f647b3aa5a4f62754e120aea259066b0785a
JLL32 <eabduljalil@outlook.com>
2023-10-18 12:37:00 +0100

    retry 4th stage 6

--- app/server.go
+++ app/server.go
@@ -37,8 +37,8 @@ func main() {
 			fmt.Fprint(conn, "HTTP/1.1 200 Ok\r\n\r\n")
 		} else if ok, _ := regexp.Match("/echo/*", []byte(head[1])); ok {
 			path := strings.Split(head[1][1:], "/")
-			if len(path) == 2 {
-				fmt.Fprint(conn, contentResponse(path[1]))
+			if len(path) >= 2 {
+				fmt.Fprint(conn, contentResponse(strings.Join(path[1:], "")))
 			} else {
 				fmt.Fprint(conn, contentResponse(""))
 			}
