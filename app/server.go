5c14a16ddfd703107496c2728bfa516eca6c8c30
JLL32 <eabduljalil@outlook.com>
2023-10-30 16:19:38 +0100

    refactoring

--- app/server.go
+++ app/server.go
@@ -5,13 +5,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
+	"os"
 	"path"
 	"strconv"
 	"strings"
-
-	// Uncomment this block to pass the first stage
-	"net"
-	"os"
 )
 
 func main() {
@@ -36,69 +34,69 @@ func main() {
 			continue
 		}
 
-		go func() {
-			defer conn.Close()
+		go serve(conn, filesDir)
+	}
 
-			req := readRequest(conn)
+}
 
-			if len(req.path) == 0 {
-				fmt.Fprint(conn, "HTTP/1.1 200 Ok\r\n\r\n")
+func serve(conn net.Conn, dir string) {
+	defer conn.Close()
+
+	req := readRequest(conn)
+
+	switch req.path[0] {
+	case "":
+		fmt.Fprint(conn, "HTTP/1.1 200 Ok\r\n\r\n")
+
+	case "echo":
+		var content string
+		if len(req.path) >= 2 {
+			content = strings.Join(req.path[1:], "/")
+		} else {
+			content = ""
+		}
+		fmt.Fprint(conn, textResponse(content))
+
+	case "user-agent":
+		fmt.Fprint(conn, textResponse(req.headers["User-Agent"]))
+
+	case "files":
+		if len(req.path) < 2 {
+			notFound(conn)
+			return
+		}
+
+		if req.verb == "POST" {
+			name := path.Join(req.path[1:]...)
+			path := path.Join(dir, name)
+			f, err := os.Create(path)
+			if err != nil {
+				notFound(conn)
 				return
 			}
 
-			switch req.path[0] {
-			case "echo":
-				var content string
-				if len(req.path) >= 2 {
-					content = strings.Join(req.path[1:], "/")
-				} else {
-					content = ""
-				}
-				fmt.Fprint(conn, contentResponse(content))
-
-			case "user-agent":
-				fmt.Fprint(conn, contentResponse(req.headers["User-Agent"]))
-
-			case "files":
-				if len(req.path) < 2 {
-					notFound(conn)
-					return
-				}
-
-				if req.verb == "POST" {
-					name := path.Join(req.path[1:]...)
-					path := path.Join(filesDir, name)
-					f, err := os.Create(path)
-					if err != nil {
-						notFound(conn)
-						return
-					}
-
-					_, err = f.Write(req.body)
-					if err != nil {
-						notFound(conn)
-						return
-					}
-
-					fmt.Fprint(conn, "HTTP/1.1 201 Created\r\n\r\n")
-				} else {
-					name := path.Join(req.path[1:]...)
-					path := path.Join(filesDir, name)
-					buff, err := os.ReadFile(path)
-					if err != nil {
-						notFound(conn)
-						return
-					}
-
-					fmt.Fprint(conn, fileResponse(string(buff)))
-				}
-
-			default:
+			_, err = f.Write(req.body)
+			if err != nil {
 				notFound(conn)
+				return
 			}
-		}()
-	}
 
+			fmt.Fprint(conn, "HTTP/1.1 201 Created\r\n\r\n")
+		} else {
+			name := path.Join(req.path[1:]...)
+			path := path.Join(dir, name)
+			buff, err := os.ReadFile(path)
+			if err != nil {
+				notFound(conn)
+				return
+			}
+
+			fmt.Fprint(conn, fileResponse(string(buff)))
+		}
+
+	default:
+		notFound(conn)
+	}
 }
 
 type request struct {
@@ -110,7 +108,7 @@ type request struct {
 }
 
 func readRequest(r io.Reader) request {
-	var req []string
+	var lines []string
 	reader := bufio.NewReader(r)
 	for {
 		line, err := reader.ReadString('\n')
@@ -118,61 +116,57 @@ func readRequest(r io.Reader) request {
 			break
 		}
 
-		line = strings.TrimSuffix(line, "\r\n")
-		if string(line) == "" {
+		if string(line) == "\r\n" {
 			break
 		}
 
-		req = append(req, line)
+		lines = append(lines, strings.TrimSuffix(line, "\r\n"))
 	}
 
-	if len(req) == 0 {
+	if len(lines) == 0 {
 		return request{}
 	}
 
-	var parsedRequest request
-	head := strings.Split(req[0], " ")
+	var req request
 
-	parsedRequest.verb = head[0]
+	head := strings.Split(lines[0], " ")
+
+	req.verb = head[0]
+
 	tokens := strings.Split(head[1], "/")
 	for _, v := range tokens {
 		if v != "" { // Split() split can result in empty tokens
-			parsedRequest.path = append(parsedRequest.path, v)
+			req.path = append(req.path, v)
 		}
 	}
-	parsedRequest.version = head[2]
-	if len(req) == 1 || len(req) == 2 {
-		return parsedRequest
+	if len(req.path) == 0 {
+		req.path = append(req.path, "")
 	}
 
-	i := 2
-	parsedRequest.headers = make(map[string]string)
-	for ; i < len(req) && req[i] != ""; i++ {
-		pair := strings.Split(req[i], ":")
-		fmt.Println(pair)
-		parsedRequest.headers[pair[0]] = strings.TrimSpace(pair[1])
+	req.version = head[2]
+	if len(lines) == 1 {
+		return req
 	}
 
-	l, ok := parsedRequest.headers["Content-Length"]
-	if !ok {
-		return parsedRequest
+	req.headers = make(map[string]string)
+	for i := 1; i < len(lines); i++ {
+		pair := strings.Split(lines[i], ":")
+		req.headers[pair[0]] = strings.TrimSpace(pair[1])
 	}
 
-	contentLen, err := strconv.Atoi(l)
+	contentLen, err := strconv.Atoi(
+		req.headers["Content-Length"])
 	if err != nil || contentLen == 0 {
-		return parsedRequest
+		return req
 	}
 
-	for {
+	for ; contentLen != 0; contentLen-- {
 		byte, err := reader.ReadByte()
 		if err != nil {
 			break
 		}
 
-		parsedRequest.body = append(parsedRequest.body, byte)
-		if len(parsedRequest.body) == contentLen {
-			break
-		}
+		req.body = append(req.body, byte)
 	}
 
 	// fmt.Println(parsedRequest.verb)
@@ -180,10 +174,10 @@ func readRequest(r io.Reader) request {
 	// fmt.Println(parsedRequest.version)
 	// fmt.Println(parsedRequest.headers)
 	// fmt.Println(parsedRequest.body)
-	return parsedRequest
+	return req
 }
 
-func contentResponse(content string) string {
+func textResponse(content string) string {
 	res := "HTTP/1.1 200 OK\r\n" +
 		"Content-Type: text/plain\r\n" +
 		fmt.Sprintf("Content-Length: %v\r\n", len(content)) +
